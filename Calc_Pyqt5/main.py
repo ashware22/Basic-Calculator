28cbdc724e8c3f08527d1c7b8546cda54ea0a708
Ashu <asonar989@gmail.com>
2022-03-29 22:55:44 +0530

    Initial commit

---
+++ Calc_Pyqt5/main.py
@@ -0,0 +1,10 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from calculator import CalculatorWindow
+
+app = QApplication(sys.argv)
+
+calculator = CalculatorWindow()
+
+sys.exit(app.exec_())
